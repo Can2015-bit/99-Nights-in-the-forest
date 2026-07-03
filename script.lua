583d788ead91e4f097ed3ca0f65caa0e8f4b1261
Can2015-bit <cansimdi15@gmail.com>
2026-07-03 12:18:44 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,34 +1,26 @@
--- Sadece senin (veya admin yapmak istediğin arkadaşının) UserId'si
 local Adminler = {
-    [10519495369] = true,
-    
+    [10519495369] = true, 
 }
 
 game.Players.PlayerAdded:Connect(function(player)
-    
     if Adminler[player.UserId] then
-        
-        
         player.Chatted:Connect(function(mesaj)
             local karakter = player.Character
-            
             
             if mesaj:lower() == "!bring coal" then
                 if karakter and karakter:FindFirstChild("HumanoidRootPart") then
                     local adminPozisyon = karakter.HumanoidRootPart.CFrame
                     local sayac = 0
                     
-                    -- Haritadaki tüm nesneleri tarar
                     for _, obje in ipairs(workspace:GetDescendants()) do
-                        
-                        if obje.Name == "Coal" and obje:IsA("BasePart") then
-                            
-                            obje.CFrame = adminPozisyon + Vector3.new(0, 3, 0)
+                        -- image_e85d49.png dosyasındaki gibi Model olan Coal'ları yakalıyoruz
+                        if obje.Name == "Coal" and obje:IsA("Model") then
+                            obje:PivotTo(adminPozisyon + Vector3.new(0, 3, 0))
                             sayac = sayac + 1
                         end
                     end
                     
-                    print("[Admin Abuse]: " .. sayac .. " adet kömür yanına getirildi!")
+                    print("[Admin Abuse]: " .. sayac .. " adet kömür modeli yanına getirildi!")
                 end
             end
             
